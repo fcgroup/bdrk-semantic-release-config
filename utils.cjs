521e6713d6244e88b632d225bc5f81e9ce65ce4d
Luke Fritz <lukefritz@gmail.com>
2024-04-15 03:11:42 -0500

    Revert utility library to CommonJS (fix)

--- utils.cjs
+++ utils.cjs
@@ -1,10 +1,9 @@
-import { resolve } from 'path';
-import { existsSync } from 'fs';
+const fs = require('fs');
+const path = require('path');
 
-export function findPlugin(plugins, pluginName) {
+function findPlugin(plugins, pluginName) {
   // plugins can be a string (just the name) or a tuple of string and configuration object
   const index = plugins.findIndex(plugin =>
-    // eslint-disable-next-line comma-dangle
     Array.isArray(plugin) ? plugin[0] === pluginName : plugin === pluginName
   );
 
@@ -13,20 +12,24 @@ export function findPlugin(plugins, pluginName) {
   }
 }
 
-export function findPackageJson() {
-  if (process.env.npm_package_json && existsSync(process.env.npm_package_json)) {
+function findPackageJson() {
+  if (process.env.npm_package_json && fs.existsSync(process.env.npm_package_json)) {
     // from npx
     return process.env.npm_package_json;
   }
 
   // assume that we are executing in the base of the repo
-  const packageJson = resolve(process.env.PWD, 'package.json');
-  if (existsSync(packageJson)) {
+  const packageJson = path.resolve(process.env.PWD, 'package.json');
+  if (fs.existsSync(packageJson)) {
     return packageJson;
   }
 }
 
-export function getPackageConfig() {
+function getPackageConfig() {
   const packageJson = findPackageJson();
   return require(packageJson);
 }
+
+exports.findPackageJson = findPackageJson;
+exports.findPlugin = findPlugin;
+exports.getPackageConfig = getPackageConfig;
